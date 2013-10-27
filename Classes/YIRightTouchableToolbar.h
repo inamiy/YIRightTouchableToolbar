36d292e2ef30be49476e9d15163703054148746b
Yasuhiro Inami <inamiy@gmail.com>
2013-10-28 01:08:15 +0900

    Add source code.

---
+++ Classes/YIRightTouchableToolbar.h
@@ -0,0 +1,13 @@
+//
+//  YIRightTouchableToolbar.h
+//  YIRightTouchableToolbar
+//
+//  Created by Yasuhiro Inami on 2013/10/28.
+//  Copyright (c) 2013年 Yasuhiro Inami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YIRightTouchableToolbar : UIToolbar
+
+@end
---
+++ Classes/YIRightTouchableToolbar.m
@@ -0,0 +1,63 @@
+//
+//  YIRightTouchableToolbar.m
+//  YIRightTouchableToolbar
+//
+//  Created by Yasuhiro Inami on 2013/10/28.
+//  Copyright (c) 2013年 Yasuhiro Inami. All rights reserved.
+//
+
+#import "YIRightTouchableToolbar.h"
+#import "JRSwizzle.h"
+
+#define IS_IOS_AT_LEAST(ver)    ([[[UIDevice currentDevice] systemVersion] compare:ver] != NSOrderedAscending)
+
+#if defined(__IPHONE_7_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
+#define IS_FLAT_DESIGN          IS_IOS_AT_LEAST(@"7.0")
+#else
+#define IS_FLAT_DESIGN          NO
+#endif
+
+
+@implementation YIRightTouchableToolbar
+
++ (void)load
+{
+    [YIRightTouchableToolbar jr_swizzleMethod:@selector(hitTest:withEvent:)
+                                   withMethod:@selector(YIRightTouchableToolbar_hitTest:withEvent:)
+                                        error:NULL];
+}
+
+// NOTE: method-swizzling UIToolbar directly didn't work, so subclass it instead
+- (UIView *)YIRightTouchableToolbar_hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView* hitSubview = [self YIRightTouchableToolbar_hitTest:point withEvent:event];
+    
+    if (!IS_FLAT_DESIGN || hitSubview != self) {
+        return hitSubview;
+    }
+    
+    UIView* mostRightSubview = nil;
+    CGFloat mostRightSubviewRight = 0;
+    
+    for (UIView* subview in self.subviews) {
+        
+        CGFloat subviewRight = subview.frame.origin.x+subview.frame.size.width;
+        if (subviewRight >= self.frame.size.width) {
+            continue;
+        }
+        
+        if (mostRightSubviewRight < subviewRight) {
+            mostRightSubview = subview;
+            mostRightSubviewRight = subviewRight;
+        }
+        
+    }
+    
+    if (point.x > mostRightSubviewRight) {
+        return mostRightSubview;
+    }
+    
+    return hitSubview;
+}
+
+@end
